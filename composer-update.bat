2a8b73c7aa9df5b5038b058a91c44df3ce8af7f7
Phichitchai <phichitchai.u@zensorium.com>
2020-03-16 15:37:15 +0700

    change

--- composer-update.bat
+++ composer-update.bat
@@ -1,3 +1,3 @@
-IF EXIST %cd%\web\vendor docker run -i --rm -v "%cd%\web:/app" 3dsinteractive/composer:7.1 update
-IF NOT EXIST %cd%\web\vendor docker run -i --rm -v "%cd%\web:/app" 3dsinteractive/composer:7.1 install
+IF EXIST %cd%\web\vendor docker run -i --rm -v "%cd%\web:/app" jengweb/composer:7.1 update
+IF NOT EXIST %cd%\web\vendor docker run -i --rm -v "%cd%\web:/app" jengweb/composer:7.1 install
 cls
--- composer-update.sh
+++ composer-update.sh
@@ -3,7 +3,7 @@
 DIR=$(pwd)
 if [ ! -d $DIR/web/vendor ]
 then
-    docker run -i --rm -v "$DIR/web:/app" 3dsinteractive/composer:7.1 install
+    docker run -i --rm -v "$DIR/web:/app" jengweb/composer:7.1 install
 else
-    docker run -i --rm -v "$DIR/web:/app" 3dsinteractive/composer:7.1 update
+    docker run -i --rm -v "$DIR/web:/app" jengweb/composer:7.1 update
 fi
